f3ed9bc114ed7d3ca306385787964284894a8f3e
taniasharma <t23sharm@uwaterloo.ca>
2019-07-12 15:34:49 -0400

    added files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(5059, () => {
+ console.log("Server running on port 5059");
 });
 
-app.get("/url", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-});
+
+app.get('/', function(req, res){
+        res.sendFile(path + 'index.html');
+        })
